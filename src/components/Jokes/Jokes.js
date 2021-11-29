3fa504657f159c0dc7e0571431def1b861d1de71
Lorien24 <tokillaggnore@gmail.com>
2021-11-29 20:04:51 +0200

    fix bugs

--- src/components/Jokes/Jokes.js
+++ src/components/Jokes/Jokes.js
@@ -2,49 +2,78 @@ import React from "react";
 import message from "../../assets/message.png";
 import like from "../../assets/like.png";
 import heart from "../../assets/heart.png";
-import "./Jokes.css"
+import "./Jokes.css";
 
-const Jokes = ({ jokes, isCliked,  handleDate, storage, getLiked}) => {
-    return(
+const Jokes = ({ jokes, isCliked, handleDate, storage, getLiked }) => {
+  return (
     <div>
-    {jokes.value === undefined && isCliked ? jokes.map(joke => 
-        <div className="joke" key={joke.id} >
+      {jokes.value === undefined && isCliked ? (
+        jokes.map((joke) => (
+          <div className="joke" key={joke.id}>
+            <div className="joke_block">
+              <img className="message" src={message} alt="message" />
+              <img
+                className="like"
+                src={
+                  storage && storage.find((j) => j.id === joke.id)
+                    ? heart
+                    : like
+                }
+                alt="like"
+                id={joke.id}
+                onClick={getLiked}
+              />
+              <p className="joke_id">
+                ID:
+                <a href={`https://api.chucknorris.io/jokes/${joke.id}`}>
+                  {joke.id}
+                </a>
+              </p>
+              <p className="joke_text">{joke.value}</p>
+            </div>
+            <div className="joke_footer">
+              <p className="joke_update">
+                Last update: {handleDate(joke.updated_at)} hours ago
+              </p>
+              {joke.categories[0] ? (
+                <p className="joke_category">{joke.categories[0]} </p>
+              ) : null}
+            </div>
+          </div>
+        ))
+      ) : isCliked ? (
+        <div className="joke">
           <div className="joke_block">
             <img className="message" src={message} alt="message" />
-            <img className="like" src={storage && storage.find(j => j.id === joke.id) ? heart : like} alt="like" id={joke.id} onClick={getLiked}/>
+            <img
+              className="like"
+              src={
+                storage && storage.find((j) => j.id === jokes.id) ? heart : like
+              }
+              alt="like"
+              id={jokes.id}
+              onClick={getLiked}
+            />
             <p className="joke_id">
               ID:
-              <a href={`https://api.chucknorris.io/jokes/${joke.id}`}>
-                {joke.id}
+              <a href={`https://api.chucknorris.io/jokes/${jokes.id}`}>
+                {jokes.id}
               </a>
             </p>
-            <p className="joke_text">{joke.value}</p>
+            <p className="joke_text">{jokes.value}</p>
           </div>
-          <div  className="joke_footer">
-            <p className="joke_update">Last update: {handleDate(joke.updated_at)} hours ago</p>
-            {joke.categories[0] ? <p className="joke_category">{joke.categories[0]} </p> : null}
+          <div className="joke_footer">
+            <p className="joke_update">
+              Last update: {handleDate(jokes.updated_at)} hours ago
+            </p>
+            {jokes.categories[0] ? (
+              <p className="joke_category">{jokes.categories[0]} </p>
+            ) : null}
           </div>
         </div>
-      ) : isCliked ? 
-      <div className="joke">
-        <div className="joke_block">
-          <img className="message" src={message} alt="message" />
-          <img className="like" src={ storage && storage.find(c => c.id === jokes.id) ? heart : like } alt="like" id={jokes.id} onClick={getLiked}/>
-          <p className="joke_id">
-            ID:
-            <a href={`https://api.chucknorris.io/jokes/${jokes.id}`}>
-              {jokes.id}
-            </a>
-          </p>
-          <p className="joke_text">{jokes.value}</p>
-        </div>
-        <div className="joke_footer">
-          <p className="joke_update">Last update: {handleDate(jokes.updated_at)} hours ago</p>
-          {jokes.categories[0] ? <p className="joke_category">{jokes.categories[0]} </p> : null}
-        </div>
-      </div>
-     : null}
-  </div>
-)};
+      ) : null}
+    </div>
+  );
+};
 
 export default Jokes;
